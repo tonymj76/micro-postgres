8f7c36c7a35584a4fa992ca8d3bb34d82c483164
tonymj76 <demeyortj@gmail.com>
2020-05-10 23:31:12 +0100

    hot fix

--- datastore/helpers.go
+++ datastore/helpers.go
@@ -2,11 +2,16 @@ package datastore
 
 import (
 	"database/sql"
+	"errors"
 
+	"github.com/Masterminds/squirrel"
 	migrate "github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	bindata "github.com/golang-migrate/migrate/v4/source/go_bindata"
+	pbError "github.com/micro/go-micro/v2/errors"
 	"github.com/tonymj76/micro-postgres/datastore/migrations"
+	pbUser "github.com/tonymj76/micro-postgres/proto/user"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // version defines the current migration version. This ensures the app
@@ -32,4 +37,23 @@ func validateSchema(db *sql.DB) error {
 		return err
 	}
 	return sourceInstance.Close()
+}
+
+//ScanUser _
+func ScanUser(row squirrel.RowScanner) (*pbUser.User, error) {
+	user := pbUser.User{}
+	user.CreatedAt = new(timestamppb.Timestamp)
+	err := row.Scan(
+		&user.Id,
+		(*timeWrapper)(user.CreatedAt),
+		(rowWrapper)(user.Role),
+	)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, pbError.BadRequest("404", "no user found", err)
+		}
+		return nil, err
+	}
+	return &user, nil
 }
