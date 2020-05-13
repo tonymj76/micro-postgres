e1c21bbad458972e5bf4b3fed834376907341a7b
tonymj76 <demeyortj@gmail.com>
2020-05-13 21:58:54 +0100

    types of db

---
+++ datastore/types.go
@@ -0,0 +1,84 @@
+package datastore
+
+import (
+	"database/sql/driver"
+	"fmt"
+
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/duration"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/jackc/pgtype"
+	"github.com/tonymj76/micro-postgres/proto/user"
+)
+
+type roleWrapper user.Role
+
+// Value implements database/sql/driver.Valuer for pbUsers.Role
+func (rw roleWrapper) Value() (driver.Value, error) {
+	switch user.Role(rw) {
+	case user.Role_ADMIN:
+		return "admin", nil
+	case user.Role_GUEST:
+		return "guest", nil
+	case user.Role_MEMBER:
+		return "memeber", nil
+	default:
+		return nil, fmt.Errorf("invalid Role: %q", rw)
+	}
+}
+
+// Scan implements database/sql/driver.Scanner for pbUsers.Role
+func (rw *roleWrapper) Scan(in interface{}) error {
+	switch in.(string) {
+	case "admin":
+		*rw = roleWrapper(user.Role_ADMIN)
+		return nil
+	case "guest":
+		*rw = roleWrapper(user.Role_GUEST)
+		return nil
+	case "memeber":
+		*rw = roleWrapper(user.Role_MEMBER)
+		return nil
+	default:
+		return fmt.Errorf("invalid Role: %q", in.(string))
+	}
+}
+
+type timeWrapper timestamp.Timestamp
+
+// Value implements database/sql/driver.Valuer for timestamp.Timestamp
+func (tw *timeWrapper) Value() (driver.Value, error) {
+	return ptypes.Timestamp((*timestamp.Timestamp)(tw))
+}
+
+// Scan implements database/sql/driver.Scanner for timestamp.Timestamp
+func (tw *timeWrapper) Scan(in interface{}) error {
+	var t pgtype.Timestamptz
+	err := t.Scan(in)
+	if err != nil {
+		return err
+	}
+	tp, err := ptypes.TimestampProto(t.Time)
+	if err != nil {
+		return err
+	}
+	*tw = (timeWrapper)(*tp)
+	return nil
+}
+
+type durationWrapper duration.Duration
+
+// Value implements database/sql/driver.Valuer for duration.Duration
+func (dw *durationWrapper) Value() (driver.Value, error) {
+	d, err := ptypes.Duration((*duration.Duration)(dw))
+	if err != nil {
+		return nil, err
+	}
+
+	i := pgtype.Interval{
+		Microseconds: int64(d) / 1000,
+		Status:       pgtype.Present,
+	}
+
+	return i.Value()
+}
