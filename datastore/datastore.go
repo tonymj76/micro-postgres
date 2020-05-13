c9288282dcc8bcd4ce0d0bba067f7b4fae58498b
tonymj76 <demeyortj@gmail.com>
2020-05-13 21:57:46 +0100

    conntion to db

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -2,12 +2,13 @@ package datastore
 
 import (
 	"database/sql"
-	"os"
+	"log"
 
 	"github.com/Masterminds/squirrel"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/log/logrusadapter"
 	"github.com/jackc/pgx/v4/stdlib"
+	"github.com/micro/go-micro/v2/config"
 	"github.com/sirupsen/logrus"
 )
 
@@ -18,9 +19,19 @@ type Connection struct {
 	SB     squirrel.StatementBuilderType
 }
 
+func loadConfig() string {
+	err := config.LoadFile("config/config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	src := config.Map()
+	conf := src["database"].(map[string]interface{})["source"].(string)
+	return conf
+}
+
 //NewConnection open a connection to db
 func NewConnection(logger *logrus.Logger) (*Connection, error) {
-	conn, err := pgx.ParseConfig(os.Getenv("CONNSTR"))
+	conn, err := pgx.ParseConfig(loadConfig())
 	if err != nil {
 		return nil, err
 	}
