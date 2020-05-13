86db84a532b9c4dafa5adbcf0796650a60524ca9
tonymj76 <demeyortj@gmail.com>
2020-05-13 21:59:29 +0100

    sub user

---
+++ subscriber/user.go
@@ -0,0 +1,24 @@
+package subscriber
+
+// import (
+// 	"context"
+
+// 	log "github.com/micro/go-micro/v2/logger"
+// 	pbUser "github.com/tonymj76/micro-postgres/proto/user"
+// )
+
+// //User _
+// type User struct{}
+
+// //Handle _
+// func (e *User) Handle(ctx context.Context, msg *pbUser.ProtoMessage) error {
+// 	log.Info("Handler Received message: ", msg.Say)
+// 	pbUser.P
+// 	return nil
+// }
+
+// //Handler _
+// func Handler(ctx context.Context, msg *pbUser.Message) error {
+// 	log.Info("Function Received message: ", msg.Say)
+// 	return nil
+// }
