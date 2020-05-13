1953b5de93ea4c9f0f9dd4f310959e832d2abbc5
tonymj76 <demeyortj@gmail.com>
2020-05-13 21:50:51 +0100

    main file

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"time"
+
+	"github.com/micro/go-micro/v2"
+	"github.com/sirupsen/logrus"
+	"github.com/tonymj76/micro-postgres/datastore"
+	"github.com/tonymj76/micro-postgres/handler"
+	pbUser "github.com/tonymj76/micro-postgres/proto/user"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("service.user"),
+		micro.Version("0.1"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	log := logrus.New()
+	log.Formatter = &logrus.TextFormatter{
+		TimestampFormat: time.StampMilli,
+		FullTimestamp:   true,
+	}
+	conn, err := datastore.NewConnection(log)
+	log.WithError(err).Fatal("database faild to connect")
+	// Register Handler
+	pbUser.RegisterUserServiceHandler(service.Server(), &handler.Service{R: conn})
+
+	// Register Struct as Subscriber
+	// micro.RegisterSubscriber("service.user", service.Server(), new(subscriber.User))
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
