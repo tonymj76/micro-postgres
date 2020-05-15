cc4f77caa4658ef71c5787b572db030fccb012d0
tonymj76 <demeyortj@gmail.com>
2020-05-15 12:44:39 +0100

    explicit err

--- handler/user.go
+++ handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 
+	"github.com/micro/go-micro/v2/errors"
 	"github.com/tonymj76/micro-postgres/datastore"
 	pbUser "github.com/tonymj76/micro-postgres/proto/user"
 )
@@ -15,16 +16,26 @@ type Service struct {
 //AddUser _
 func (s *Service) AddUser(ctx context.Context, req *pbUser.AddUserRequest, res *pbUser.User) (err error) {
 	res, err = s.R.Create(ctx, req)
-	return
+	if err != nil {
+		return errors.InternalServerError("501", "service.user.AddUser", err.Error())
+	}
+	return nil
 }
 
-//DelectUser _
-func (s *Service) DelectUser(ctx context.Context, req *pbUser.DeleteUserRequest, res *pbUser.User) (err error) {
+//DeleteUser _
+func (s *Service) DeleteUser(ctx context.Context, req *pbUser.DeleteUserRequest, res *pbUser.User) (err error) {
 	res, err = s.R.Delete(ctx, req)
-	return
+	if err != nil {
+		return errors.InternalServerError("501", "service.user.DeleteUser", err.Error())
+	}
+	return nil
 }
 
 //ListUsers _
 func (s *Service) ListUsers(ctx context.Context, req *pbUser.ListUsersRequest, stream pbUser.UserService_ListUsersStream) error {
-	return s.R.List(ctx, req, stream)
+	err := s.R.List(ctx, req, stream)
+	if err != nil {
+		return errors.InternalServerError("501", "service.user.ListUsers", err.Error())
+	}
+	return nil
 }
